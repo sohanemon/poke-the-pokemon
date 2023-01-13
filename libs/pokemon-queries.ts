a679c61e82739ae2863148570ca8542c57b303a2
sohanemon <m.sohanemon@gmail.com>
2023-01-13 20:42:47 +0600

    queries updated

---
+++ libs/pokemon-queries.ts
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const GET_10_POKEMONS = gql`
+  query {
+    pokemons(limit: 10) {
+      results {
+        name
+        image
+      }
+    }
+  }
+`;
