eb3ee34144ffca37772ab9fd0b2028c900509d60
sohanemon <m.sohanemon@gmail.com>
2023-01-13 16:15:15 +0600

    card added

--- components/card.tsx
+++ components/card.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+import image from "@/assets/media/image04.png";
+
 export default function Card() {
-  return <></>;
+  return (
+    <div className='w-60 h-80 bg-white p-[10px]'>
+      <Image className='w-full h-52' src={image} alt='' />
+      <p className='text-base'>Bulbasaur</p>
+    </div>
+  );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -2,15 +2,22 @@ import bg from "@/assets/media/Background.png";
 // import bg from "../assets/media/Background.png";
 import logo from "@/assets/media/logo.png";
 import Image from "next/image";
+import Card from "./card";
 export default function Hero() {
   return (
     <section
       className='min-h-screen w-full !bg-cover'
       style={{ background: `url(${bg.src})`, backgroundRepeat: "no-repeat" }}
     >
-      {/* done: logo */}
-      <div className='flex justify-center '>
+      <div className='flex flex-col items-center '>
+        {/* done: logo */}
         <Image src={logo} alt='Logo' className='my-16' />
+        {/* todo: card */}
+        <div className='grid grid-cols-5 gap-24 '>
+          {[...Array(10).keys()].map((card, idx) => (
+            <Card key={idx} />
+          ))}
+        </div>
       </div>
     </section>
   );
