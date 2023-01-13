4b210f62826c5a3c7dc09d5c458793a7ec5be74f
sohanemon <m.sohanemon@gmail.com>
2023-01-13 17:18:53 +0600

    hover implemented

--- components/card.tsx
+++ components/card.tsx
@@ -4,13 +4,13 @@ import image from "@/assets/media/image04.png";
 export default function Card() {
   return (
     <div
-      className=' bg-white p-[10px] rounded-md'
+      className=' bg-white p-[10px] rounded-md hover:bg-primary anim cursor-pointer group'
       style={{
         clipPath: "polygon(0% 0%, 100% 0%, 100% 80%, 80% 100%, 0% 100%)",
       }}
     >
-      <Image className='w-full ' src={image} alt='' />
-      <p className='mt-3 mb-1'>Bulbasaur</p>
+      <Image className='w-full hover:scale-110 anim' src={image} alt='' />
+      <p className='mt-3 mb-1 group-hover:text-white anim'>Bulbasaur</p>
       <div className='flex gap-2 2xl:gap-2 p-[6px] mr-4'>
         <button className='bg-grass btn'>Grass</button>
         <button className='bg-poison btn'>Poison</button>
