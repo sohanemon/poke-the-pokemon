9a006841d51ac2c7b93aee038bcebbfecadd3dec
sohanemon <m.sohanemon@gmail.com>
2023-01-13 19:02:11 +0600

    wrapped

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,24 @@
-import './globals.css'
-
+import "./globals.css";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+});
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ApolloProvider client={client}>{children}</ApolloProvider>
+      </body>
     </html>
-  )
+  );
 }
