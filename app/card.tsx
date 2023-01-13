08ac9385f98d64c8b09e032bc2d75cdf75978b0b
sohanemon <m.sohanemon@gmail.com>
2023-01-13 20:49:34 +0600

    sim card updated

--- app/card.tsx
+++ app/card.tsx
@@ -17,7 +17,9 @@ export default function Card({ pokemon }: { pokemon: Pokemon }) {
           />
         </div>
         <p className='text-sm absolute inset-4'>#010</p>
-        <p className='mt-3 mb-1 group-hover:text-white anim'>{pokemon.name}</p>
+        <p className='mt-3 mb-1 group-hover:text-white anim capitalize'>
+          {pokemon.name}
+        </p>
         <div className='flex gap-2 2xl:gap-2 p-[6px] mr-4'>
           <button className='bg-grass btn'>Grass</button>
           <button className='bg-poison btn'>Poison</button>
