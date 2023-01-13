71902d9cadc7199d78865f09b8ac39a98b5f7dde
sohanemon <m.sohanemon@gmail.com>
2023-01-13 20:42:37 +0600

    props passed

--- app/hero.tsx
+++ app/hero.tsx
@@ -5,12 +5,13 @@ import { useQuery } from "@apollo/client";
 
 import Logo from "../components/logo";
 import Card from "./card";
-import { GET_POKEMONS, gqlVariables } from "../libs/get-pockemon";
+import { GET_10_POKEMONS } from "../libs/pokemon-queries";
 export default function Hero() {
-  const { loading, error, data } = useQuery(GET_POKEMONS, {
-    variables: gqlVariables,
-  });
-  console.log(data);
+  const { loading, error, data } = useQuery(GET_10_POKEMONS);
+
+  if (error) {
+    return <div className='text-7xl text-center'>Unexpected Error</div>;
+  }
   return (
     <section
       className='min-h-screen bg-no-repeat w-full bg-cover bg-center'
@@ -22,8 +23,8 @@ export default function Hero() {
         {/* todo: card */}
         <div className='grid lg:grid-cols-4 xl:grid-cols-5 gap-10 w-5/6 xl:w-4/5 2xl:w-2/3 items-center mb-40'>
           {/* @ts-ignore */}
-          {[...Array(10).keys()].map((card, idx) => (
-            <Card key={idx} />
+          {data?.pokemons?.results.map((pokemon: Pokemon, idx) => (
+            <Card key={idx} pokemon={pokemon} />
           ))}
         </div>
       </div>
