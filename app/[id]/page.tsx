181769afbf30f4a32b6f09cab2def07dac8a060d
sohanemon <m.sohanemon@gmail.com>
2023-01-13 17:29:51 +0600

    dynamic route added

---
+++ app/[id]/page.tsx
@@ -0,0 +1,14 @@
+import Logo from "@/components/logo";
+
+export default function Page() {
+  return (
+    <div className='flex flex-col items-center'>
+      <Logo />
+      <main>
+        {/* todo: left */}
+        {/* todo: middle */}
+        {/* todo: right */}
+      </main>
+    </div>
+  );
+}
