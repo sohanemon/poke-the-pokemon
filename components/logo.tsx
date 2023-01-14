a86daeab127da8da1c97fa81ae24df420547fcf3
sohanemon <m.sohanemon@gmail.com>
2023-01-14 22:32:34 +0600

    logo z

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Link href={"/"}>
+    <Link href={"/"} className='z-50'>
       <Image src={logo} alt='Logo' className='my-16' />
     </Link>
   );
