f39db5b6609a19b08c1f785be402ba111428ce2a
sohanemon <m.sohanemon@gmail.com>
2023-01-14 22:15:53 +0600

    reorriented

--- app/[pokemon]/button.tsx
+++
@@ -1,9 +0,0 @@
-export default function Button({ type }: { type: Type }) {
-  return (
-    <>
-      <button className={` capitalize  bg-${type?.type?.name} btn`}>
-        {type.type.name}
-      </button>
-    </>
-  );
-}
