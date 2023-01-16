3ad2123b0fbde7c4d14f3d6577c30ffe306356be
sohanemon <m.sohanemon@gmail.com>
2023-01-16 12:01:01 +0600

    mobile grid done

--- app/blog.tsx
+++ app/blog.tsx
@@ -29,7 +29,7 @@ export default function Blog() {
         aliquam. Tincidunt semper ut ornare vivamus lectus.
       </p>
       {/* todo: grid image section */}
-      <div className='grid grid-cols-3 grid-rows-5 gap-3'>
+      <div className='grid grid-cols-3 grid-rows-5 gap-3 my-5'>
         <Image
           className='col-start-2 row-span-2 object-cover h-full'
           src={img1}
@@ -57,6 +57,16 @@ export default function Blog() {
           alt='img'
         />
       </div>
+      {/* second details */}
+      <p className='text-justify'>
+        Lorem ipsum dolor sit amet consectetur. Risus cursus nibh elementum
+        ornare a aliquet ac. Feugiat scelerisque ultrices tempor facilisi tempus
+        risus nunc. Proin quis morbi posuere nisl etiam scelerisque. Proin
+        pretium gravida semper ut erat nisi. Pulvinar ac mattis porta amet et.
+        Nisl urna non fames felis leo. Vitae pulvinar sed viverra sit pretium
+        lorem elementum. Iaculis sit maecenas sodales mi convallis justo
+        aliquam. Tincidunt semper ut ornare vivamus lectus.
+      </p>
     </section>
   );
 }
