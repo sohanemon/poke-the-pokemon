8af766c70fe7fb1188d21db84db9f33182ede4d7
sohanemon <m.sohanemon@gmail.com>
2023-01-16 11:59:07 +0600

    grid implemented successfully

--- app/blog.tsx
+++ app/blog.tsx
@@ -1,11 +1,15 @@
 import Brush from "@/assets/media/Brush.png";
 import Image from "next/image";
-
+import img1 from "@/assets/media/Image01.png";
+import img2 from "@/assets/media/Image02.png";
+import img3 from "@/assets/media/Image03.png";
+import img4 from "@/assets/media/Image04.png";
+import img5 from "@/assets/media/Image05.png";
 export default function Blog() {
   return (
     <section className='py-8 px-5'>
       {/* done: heading */}
-      <div className='relative' style={{ backgroundImage: `url(${Brush})` }}>
+      <div className='relative flex justify-center'>
         <Image src={Brush} alt='brush' className='scale-50' />
         <div className='absolute top-6 flex w-full justify-center'>
           <p className='text-2xl font-bold text-center text-primary '>
@@ -24,6 +28,35 @@ export default function Blog() {
         lorem elementum. Iaculis sit maecenas sodales mi convallis justo
         aliquam. Tincidunt semper ut ornare vivamus lectus.
       </p>
+      {/* todo: grid image section */}
+      <div className='grid grid-cols-3 grid-rows-5 gap-3'>
+        <Image
+          className='col-start-2 row-span-2 object-cover h-full'
+          src={img1}
+          alt='img'
+        />
+
+        <Image
+          className='col-start-1 row-span-2 object-cover h-full'
+          src={img2}
+          alt='img'
+        />
+        <Image
+          className='row-span-2 h-full object-cover'
+          src={img3}
+          alt='img'
+        />
+        <Image
+          className='row-span-2 h-full object-cover'
+          src={img4}
+          alt='img'
+        />
+        <Image
+          className='col-start-3 row-span-2 h-full object-cover'
+          src={img5}
+          alt='img'
+        />
+      </div>
     </section>
   );
 }
